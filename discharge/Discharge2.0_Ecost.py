3ce4fc2f6073a35f87961087ee3a0577bcd3e61a
tommyesse95 <t.scali@student.tudelft.nl>
2019-12-02 19:22:03 +0100

    Discharge

--- discharge/Discharge2.0_Ecost.py
+++ discharge/Discharge2.0_Ecost.py
@@ -11,53 +11,32 @@ import numpy as np
 from numpy import linalg as LA
 from scipy import interpolate
 import random
-from general_data import *
 import cross_sections as cr
 
+# Definition of constants
 
-#def csv_reader(filename):
-#    data = np.genfromtxt(filename,delimiter=',')
-#    return data
+epsilon_0 = 8.8e-12 
+mass_Kr = 84*1.6e-27 # kg 
+elementary_charge = 1.6e-19
+electron_max_cx_tot = 3.7e-19 #Maximum total cross section for the interactions involved.
+E_exc = 9.96*elementary_charge
+E_ion = 13.9996*elementary_charge
+T_neutr = 300
+kb = 1.380649e-23
+electron_mass = 9.1e-31
+electron_max_cx_tot = 3.7e-19
+T_rod = 2000 # tempreature wire
+A_kr = 16
+B_kr = 240
+sec_coeff = 0.1 # number of secondary electrons produced per incident positive ion
 
-
-#def E_field_interpolator(E_field,coord):
-#    f = interpolate.interp2d(E_field[:,1],E_field[:,2],E_field[:,(3+coord)],kind='linear')
-#    return f
-
-
-#def E_field_interpolator3(E_field,coord,position):
-#    position_y = position[0]
-#    position_z = position[1]
-#    if(np.abs(position_y) > 0.0144):
-#        if (position_y > 0):
-#            position_y = 0.0144
-#        else:
-#            position_y = -0.0144
-#    f = interpolate.griddata(E_field[:,1:3],E_field[:,(3+coord)],(np.abs(position_y),position_z),method='linear')
-#    if(position_y < 0.0 and coord == 1):
-#        f = -f
-#    if(f > 1e5):
-#        f = 1e5
-#    elif(f < -1e5):
-#        f = -1e5
-#    return f
 
 
 
 def temp_to_vel(temperature,mass_particle):
-    v_th = np.sqrt(0.5*k_B*temperature/mass_particle)
+    v_th = np.sqrt(2*k_B*temperature/mass_particle)
     return v_th
 
-
-#def E_field_interpolator2(Voltage_diff,coord):
-#        anode_position = np.array([0.0,1.5e-3,0.0])
-#        if (coord == 0):
-#            f = 0
-#        elif(coord == 1):
-#            f = Voltage_diff/(anode_position[1])
-#        elif(coord == 2):
-#            f = 0
-#        return f
 
 
 class particle:
@@ -100,18 +79,6 @@ def Maxwel_sample(v_th):
     return v_th*f_M
 
 
-#NOte this needs to be updated in case of actual electric field
-#def position_checker(particle):
-#    if np.abs(particle.position[1]) > 0.015:
-#        return True
-#    elif (particle.position[2] < 0.00 or particle.position[2] > 0.1):
-#        return True
-#    elif (np.abs(particle.position[1]) > 0.04):
-#        return True
-#    else:
-#        return False
-
-
 def position_checker(particle, d_plate):
     if np.abs(particle.position[1]) > d_plate:
         return True
@@ -127,21 +94,17 @@ def pusher(particle,delta_t,mass_electron,charge_electron,E_field):
     pos_old = particle.position
     vel_new = np.zeros(3)
     pos_new = np.zeros(3)
-#    vel_new[0] = -elementary_charge*E_field_interpolator3(E_field,0,(particle.position[1],particle.position[2]))/mass_electron*delta_t+vel_old[0]
-#    vel_new[1] = -elementary_charge*E_field_interpolator3(E_field,1,(particle.position[1],particle.position[2]))/mass_electron*delta_t+vel_old[1]
-#    vel_new[2] = -elementary_charge*E_field_interpolator3(E_field,2,(particle.position[1],particle.position[2]))/mass_electron*delta_t+vel_old[2]
     vel_new[0] = -elementary_charge*E_field[0]/mass_electron*delta_t+vel_old[0]
     vel_new[1] = -elementary_charge*E_field[1]/mass_electron*delta_t+vel_old[1]
     vel_new[2] = -elementary_charge*E_field[2]/mass_electron*delta_t+vel_old[2]
-
     pos_new[0] = pos_old[0]+vel_new[0]*delta_t
     pos_new[1] = pos_old[1]+vel_new[1]*delta_t
     pos_new[2] = pos_old[2]+vel_new[2]*delta_t
     particle.new_position(pos_new)
     particle.new_velocity(vel_new)
+    
 
 def does_it_collide(neutral_density,delta_x,electron_energy):
-    # probability = 1 - np.exp(-neutral_density*electron_tot_cx(electron_energy)*delta_t)
     probability = 1 - np.exp(-neutral_density*electron_max_cx_tot*delta_x)
     collides = (random.random() < probability)
     return collides
@@ -169,15 +132,11 @@ def elastic_vel_update(particle1,rel_vel_mod,v_center_of_mass):
     return new_vel2
 
 
-
 def e_excitation(particle1):
     v_the = temp_to_vel(300,mass_Kr)
     v2 = np.array([Maxwel_sample(v_the),np.abs(Maxwel_sample(v_the)),Maxwel_sample(v_the)])
     red_mass = electron_mass*mass_Kr/(electron_mass+mass_Kr)
-    rel_vel_mod = np.abs(LA.norm((particle1.velocity-v2))**2-2*E_exc/red_mass)
-    if rel_vel_mod < 0.0:
-        print("PROBLEMS")
-    rel_vel_mod = np.sqrt(rel_vel_mod)
+    rel_vel_mod = np.sqrt(np.abs(LA.norm((particle1.velocity-v2))**2-2*E_exc/red_mass))
     v_center_of_mass = (particle1.velocity*electron_mass+v2*mass_Kr)/(electron_mass+mass_Kr)
     elastic_vel_update(particle1,rel_vel_mod,v_center_of_mass)
 
@@ -227,154 +186,118 @@ def distance_pd(pd,pressure):
     return distance
 
 
-num_sim = 3
 
-for j in range(num_sim):
-    print(f"Simulation {j+1} Started \n")
-    number_of_Electrons =  np.array([500,700,1000])
-    num_of_iter = 10
-    T_neutr = 300
-    kb = 1.380649e-23
-    electron_mass = 9.1e-31
-    elementary_charge = 1.6e-19
-    electron_max_cx_tot = 3.7e-19
-    T_rod = 2000 # tempreature wire
-    v_thermal = temp_to_vel(T_rod,electron_mass)
-    delta_t = np.array([1e-12,1e-12,1e-12]) # time step simulation
-    Voltage = ([250,400,500])
-    pd = np.array([5, 5, 5]) # Torr*cm
-    pd_si = pd[j] * 133.322 * 1e-2 # conversion in Pa*m
-    d_plate = np.array([5e-4, 2e-3, 2e-2])  # distance between parallel plates
-    pressure = pressure_pd(pd_si, d_plate[j]) # Pressure in Pascal considering pd
-    n0 = pressure_to_density(pressure,kb,T_neutr) #density of the neutral gas
+number_of_Electrons = 100  
+num_of_iter = 10 #tba  
+v_thermal = temp_to_vel(T_rod,electron_mass)
+delta_t = 1e-12 # tba I set this because with those data we were in that part of curve. 
+d_plate = 2e-3 # tba I set this, but maybe with lower voltage it takes a while
+pd = np.array([5, 5, 6]) # tba 
+# Voltage = np.linspace(250,550,10)
+Voltage = np.arange(270,540,10) # tba 
 
-    lambda_free = 1 / (electron_max_cx_tot * n0) # mean free path
-    est_in_vel = np.sqrt((2*Voltage[j]*elementary_charge)/(electron_mass)) #Velocity for conservation energy
-    deltat_free = lambda_free/est_in_vel # delta t needed for distance lamda free
-    factor_distance = d_plate[j] / lambda_free
-    print(f"dt for mean free path: {deltat_free}")
-
-    Ey = -Voltage[j] / d_plate[j]
-    E_field = np.array([0, Ey, 0])
-
-    A_kr = 16
-    B_kr = 240
-    sec_coeff = 0.1 # number of secondary electrons produced per incident positive ion
-    M = 1 + 1/sec_coeff
-
-
-    # V_br= (pd_si * B_kr**(2)) / (np.log(M/(A_kr*pd_si)))**(2) source
-
-    # wikipedia source
-    V_br = (B_kr*pd_si) / (np.log(A_kr*pd_si) - np.log(np.log(1+ 1/sec_coeff)))
-
-
-    num_excitations = 0 # Inizialitation number of excitations
-    num_ionizations = 0 # Initialization number of ionizations
-    num_scatterings = 0
-    no_collisions = 0
-    nothing = 0
-    time_in_domain = 0
-    out_of_bounds = False
-
-    for i in range(number_of_Electrons[j]):
-
-        print("Random Initialization of the swarm \n")
-        random_angle = (random.random()*np.pi) # Angle of departure for electorns
-    #        shift_of_particle = np.array([0.0,rod_radius*(1.01)*np.sin(random_angle),rod_radius*(1.1)*np.cos(random_angle)]) # tba! why those numbers?
-    #        starting_position = rod_position
-        starting_position = np.array([0.0, 0.0, 0.0])
-        electron_swarm = swarm("electrons")
-        electron = particle(starting_position)
-        v_thermal = temp_to_vel(T_rod,electron_mass) # Thermal velocity electrons
-        vel = np.array([Maxwel_sample(v_thermal),np.abs(Maxwel_sample(v_thermal)),Maxwel_sample(v_thermal)])
-        electron.new_velocity(vel)
-        electron_swarm.add_particle(electron)
-
-
-    for k in range(num_of_iter):
-        sec_emission = round(sec_coeff * num_ionizations)
-        if sec_emission > number_of_Electrons[j]:
-            time = time_in_domain
-            print("Breakdown! after {time_in_domain * delta_t[j]} s\n")
-            break
-        for p in range(electron_swarm.size()):
-            out_of_bounds = position_checker(electron_swarm.particle_Array[p], d_plate[j])
-            while not out_of_bounds:
-                pusher(electron_swarm.particle_Array[p],delta_t[j],electron_mass,-1.0*elementary_charge,E_field)
-                Energy_part = 0.5*electron_swarm.particle_Array[p].velSquared()*electron_mass
-                Energy_electron = Energy_part / elementary_charge
-                out_of_bounds = position_checker(electron_swarm.particle_Array[p], d_plate[j])
-                delta_x = delta_t[j] * np.sqrt(electron_swarm.particle_Array[p].velSquared())
-                if(does_it_collide(n0,delta_x,Energy_part)):
-                    rand = random.random()
-                    if(rand < cr.electron_exc_rel_cx(Energy_electron)):
-                        e_excitation(electron_swarm.particle_Array[p])
-                        num_excitations += 1
-                        print(f"Electron {p+1}: excitation, with energy: {Energy_part}")
-                    elif(rand <(cr.electron_exc_rel_cx(Energy_electron)+cr.electron_ion_rel_cx(Energy_electron))):
-                        e_ionization(electron_swarm.particle_Array[p],electron_swarm)
-                        num_ionizations += 1
-                        print(f"Electron {p+1}: IONIZATION, with energy: {Energy_part}")
-                    elif(rand <(cr.electron_exc_rel_cx(Energy_electron)+cr.electron_ion_rel_cx(Energy_electron)+cr.electron_elas_scat_cx(Energy_electron))):
-                        e_elastic(electron_swarm.particle_Array[p])
-                        num_scatterings += 1
-    #                       print(f"Electron {p+1}: scattering with energy: {Energy_part}")
-                    else:
-                        nothing += 1
-                else:
-                    print(f"Electron {p}: No collisions")
-                    no_collisions += 1
-
-                time_in_domain +=1
-
-    sec_emission = round(sec_coeff * num_ionizations)
-
-    time_break = time_in_domain * delta_t[j]
-    print(f"\n\nSimulation {j+1} with {number_of_Electrons[j]} electrons", file=open("output.txt", "a"))
-
-    if sec_emission > number_of_Electrons[j]:
-            print("Breakdown! at {time_break} s", file=open("output.txt", "a"))
-
-    print(f"Pressure neutral gas: {pressure} Pa", file=open("output.txt", "a"))
-    print(f"Density neutral gas: {n0} per m\u00b3", file=open("output.txt", "a"))
-    print(f"Distance between plates: {d_plate[j]} m", file=open("output.txt", "a"))
-
-    print(f"pd = {pd_si} Pa m", file=open("output.txt", "a"))
-
+for i in range(number_of_Electrons): # tba same swarm of elec for pd?
     
-    print(f"\nTotal number of excitations: {num_excitations}", file=open("output.txt", "a"))
-    print(f"Total number of IONIZATIONS: {num_ionizations}", file=open("output.txt", "a"))
-    print(f"Total number of scatterings: {num_scatterings}", file=open("output.txt", "a"))
-    print(f"Total number of collissions wihout any phenomena: {nothing}", file=open("output.txt", "a"))
-    print(f"Total number of electrons without collisions: {no_collisions}", file=open("output.txt", "a"))
-    print(f"Total number of secondary electrons: {sec_emission}", file=open("output.txt", "a"))
-    print(f"Analytical breakdown voltage: {V_br} V", file=open("output.txt", "a"))
-    print(f"Voltage used: {Voltage[j]} V", file=open("output.txt", "a"))
+    # Initialization swarm electrons 
+    random_angle = (random.random()*np.pi) # Angle of departure for electorns
+    starting_position = np.array([0.0, 0.0, 0.0])
+    electron_swarm = swarm("electrons")
+    electron = particle(starting_position)
+    vel = np.array([Maxwel_sample(v_thermal),np.abs(Maxwel_sample(v_thermal)),Maxwel_sample(v_thermal)])
+    electron.new_velocity(vel)
+    electron_swarm.add_particle(electron)
+
+for j in range(len(pd)):
+    print(f"\n\n Simulation {j+1} with pd = {pd[j]} Torr cm \n",file=open("output.txt", "a"))
+    pd_si = pd[j] * 133.322 * 1e-2 # conversion in Pa*m
+    pressure = pressure_pd(pd_si, d_plate) # Pressure in Pascal considering pd
+    n0 = pressure_to_density(pressure,kb,T_neutr) #density of the neutral gas
+    sec_emission = 0
+
+#  V_br = (B_kr*pd_si) / (np.log(A_kr*pd_si) - np.log(np.log(1+ 1/sec_coeff)))
+
+# Check this before simulation 
+
+#    lambda_free = 1 / (electron_max_cx_tot * n0) # mean free path
+#    est_in_vel = np.sqrt((2*Voltage[j]*elementary_charge)/(electron_mass)) #Velocity for conservation energy
+#    deltat_free = lambda_free/est_in_vel # delta t needed for distance lamda free
+#    factor_distance = d_plate[j] / lambda_free
+#    print(f"dt for mean free path: {deltat_free}")
+                
+            
+    for h in range(len(Voltage)):
+        print(f"Voltage: {Voltage[h]}\n",file=open("output.txt", "a"))
+        if sec_emission > number_of_Electrons :
+            break
+        Ey = -Voltage[j] / d_plate[j]
+        E_field = np.array([0, Ey, 0])
+        # Initialization 
+        num_excitations = 0 
+        num_ionizations = 0 
+        num_scatterings = 0
+        no_collisions = 0
+        nothing = 0
+        time_in_domain = 0
+        out_of_bounds = False
+    
+        
+    
+    
+        for k in range(num_of_iter):
+            sec_emission = round(sec_coeff * num_ionizations)
+            if sec_emission > number_of_Electrons[j]:
+                time_break = time_in_domain * delta_t
+                V_break = Voltage[h]
+                print("Breakdown! after {time_break} s\n")
+                break 
+            for p in range(electron_swarm.size()):
+                out_of_bounds = position_checker(electron_swarm.particle_Array[p], d_plate)
+                while not out_of_bounds:
+                    pusher(electron_swarm.particle_Array[p],delta_t,electron_mass,-1.0*elementary_charge,E_field)
+                    Energy_part = 0.5*electron_swarm.particle_Array[p].velSquared()*electron_mass
+                    Energy_electron = Energy_part / elementary_charge
+                    out_of_bounds = position_checker(electron_swarm.particle_Array[p], d_plate)
+                    delta_x = delta_t * np.sqrt(electron_swarm.particle_Array[p].velSquared())
+                    if(does_it_collide(n0,delta_x,Energy_part)):
+                        rand = random.random()
+                        if(rand < cr.electron_exc_rel_cx(Energy_electron)):
+                            e_excitation(electron_swarm.particle_Array[p])
+                            num_excitations += 1
+                            print(f"Electron {p+1}: excitation")
+                        elif(rand <(cr.electron_exc_rel_cx(Energy_electron)+cr.electron_ion_rel_cx(Energy_electron))):
+                            e_ionization(electron_swarm.particle_Array[p],electron_swarm)
+                            num_ionizations += 1
+                            print(f"Electron {p+1}: IONIZATION}")
+                        elif(rand <(cr.electron_exc_rel_cx(Energy_electron)+cr.electron_ion_rel_cx(Energy_electron)+cr.electron_elas_scat_cx(Energy_electron))):
+                            e_elastic(electron_swarm.particle_Array[p])
+                            num_scatterings += 1
+        #                   print(f"Electron {p+1}: scattering with energy: {Energy_part}")
+                        else:
+                            nothing += 1
+                    else:
+                        print(f"Electron {p}: No collisions")
+                        no_collisions += 1
+    
+                    time_in_domain +=1
+    
+        sec_emission = round(sec_coeff * num_ionizations)
+    
+        if sec_emission > number_of_Electrons:
+            print(f"Breakdown! at {time_break} s with {V_break} V", file=open("output.txt", "a"))
+            print(f"Pressure neutral gas: {pressure} Pa", file=open("output.txt", "a"))
+            print(f"Density neutral gas: {n0} per m\u00b3", file=open("output.txt", "a"))
+            print(f"pd = {pd_si} Pa m", file=open("output.txt", "a"))
+            
+        else:
+            print("No Breakdown", file=open("output.txt", "a"))
+            
+        
+                
+       
+        
+        
+       
+        
 
 
 
-
-
-
-
-
-#def I_th(Temperature,surface):
-#    ans =surface*A_0*Lambda_R*Temperature**2.0*np.exp(-W_emitter*elementary_charge/(k_B*Temperature))
-#    return ans
-#
-#def surface(length, radius):
-#    ans = 2*np.pi*radius*length
-#    return ans
-#
-#def wire_resistance(length,radius):
-#    ans = Omega*length/(np.pi*radius**2.0)
-#    return ans
-#
-#def heat_deposit(length,radius,wire_current):
-#    ans = wire_resistance(length,radius)*wire_current**2.0
-#    return ans
-#
-#def surface_temperature(length,radius,wire_current):
-#    ans = np.sqrt(np.sqrt(heat_deposit(length,radius,wire_current)/(surface(length, radius)*SB*epsilon_emitter)+T_inf**4.0))
-#    return ans
--- starfish_data.py
+++ starfish_data.py
@@ -10,10 +10,17 @@ import numpy as np
 from decimal import Decimal
 import math as mt 
 
+kb = 1.380649e-23 #Boltzamnn Constant
+mass_elec = 9.1909e-31 # kg 
+elem_charge = 1.6e-19 # elementary charge
+mKr = 83.798 #u of Krypton
+conv = 1.66054e-27# 1u in kg
+eps0 = 8.851e-12
+
+
 # from ev to K 
 def ev_to_K(T):
-    kb = 8.611733e-5 #Boltzamnn Constant
-    T_K = T / kb
+    T_K = T * 1.160451812e4
     return T_K
 
 
@@ -21,18 +28,14 @@ def ev_to_K(T):
 
 
 def mdotelec_to_curr(mdot_elec):
-    mass_elec = 9.1909e-31 # kg 
     mdot_elec *= 1e-6 # conversion in kg from mg 
     num_elec = mdot_elec / mass_elec
-    elem_charge = 1.6e-19
     curr = num_elec * elem_charge
     return curr 
 
 
 # From mdot of neutral gas Kr, particles per dt 
-def mdot_to_kr_dt(mdot,spwt):
-    mKr = 83.798 #u of Krypton
-    conv = 1.66054e-27# 1u in kg
+def mdot_to_kr_dt(mdot,spwt,dt):
     mKr_kg = mKr * conv 
     mdot *= 1e-6 # conversion in kg from mg 
     part_s = mdot / mKr_kg
@@ -40,15 +43,11 @@ def mdot_to_kr_dt(mdot,spwt):
     part_dt = part_s_sim * dt
     return part_dt
 
-# Fot the neutral gas I can alos fix the pressure. See discharge code for 
-# computation 
-    
 
 # current --> number of electrons inside per dt. 
-def curr_to_el_dt(curr,spwt):
-    elem_charge = 1.61e-19
+def curr_to_el_dt(curr,spwt_el,dt):
     num_el_s = curr / elem_charge
-    num_el_sim_s = num_el_s / spwt
+    num_el_sim_s = num_el_s / spwt_el
     num_el_sim_dt = str(num_el_sim_s * dt)
     return num_el_sim_dt
 
@@ -58,6 +57,8 @@ def num_nodes(domain_size, spacing):
     nodesy = domain_size[1] / spacing[1]
     return (nodesx, nodesy)
 
+
+# Domain 
 domain_size = np.array([140e-3,15e-3]) # domain size in mm 
 spacing = np.array([5e-4, 5e-4]) # size of one cell
 min_nodesx, min_nodesy = num_nodes(domain_size, spacing)
@@ -65,31 +66,48 @@ min_nodesx, min_nodesy = num_nodes(domain_size, spacing)
 
 mdot_kr = 1 # mg/s
 curr_el = 2 # A 
-spwt_kr = 1e10
-spwt_el = 5e10
-dt = 5e-10
-num_it = 3000000
-time_sim = dt * num_it
+spwt_kr = 1e10 # Particles of Kr in one macroparticle 
+spwt_el = 5e10 # Particles of electrons in one macroparticle 
+num_it = 3000000 # number of iterations
+time_sim = dt * num_it # Time simulation
  
 
-print(f"Number of neutrals per dt= {dt} : " '%.2E' % Decimal(mdot_to_kr_dt(mdot_kr,spwt_kr)))
-print(f"Number of electrons per dt= {dt} : " '%.2E' % Decimal(curr_to_el_dt(curr_el,spwt_el)))
+# Number of electrons and neutrlas injected per dt 
+def num_neut_ele():
+    print(f"Number of neutrals per dt= {dt} : " '%.2E' % Decimal(mdot_to_kr_dt(mdot_kr,spwt_kr)))
+    print(f"Number of electrons per dt= {dt} : " '%.2E' % Decimal(curr_to_el_dt(curr_el,spwt_el)))
+    
+
+
+
+ 
+def drift_ele(ne,I,r_or): # ne: density electrons in the orifice
+    A = mt.pi * r_or**2
+    drift_v = I / (ne* elem_charge * A) # drift velocity of electrons 
+    print(f" Drift velocity of electrons in the orifice: {drift_v}")
+
+
+# Thermal velocity 
+def temp_to_vel(temperature,mass_particle):
+    v_th = np.sqrt(2*kb*temperature/mass_particle)
+    return v_th
+
+# Density from pressure. 
+def pressure_to_density(pressure,kb,T_neutr):
+    density = pressure/(kb*T_neutr)
+    return density
+
+# Debye lenght
+def debye(Te,n0):
+    debye = np.sqrt((eps0*kb*ev_to_K(Te))/(n0*elem_charge**2))
+    return debye
 
 
 
 
-# Drift of electrons in the orifice
 
-ne = 1e20 # density electrons in the orifice in m -3 
-I = 10 # discharge current 
-r_orifice = 2e-3 # mm 
-A = mt.pi * r_orifice**2
-elem_charge = 1.61e-19
-drift_v = I / (ne* elem_charge * A) # drift velocity of electrons 
 
-# THe thermal velocity is much bigger. 
 
-print(f" Drift velocity of electrons in the orifice: {drift_v}")
 
 
 
