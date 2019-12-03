0ec4ddb23be69063182d92424289343a93a5c027
tommyesse95 <t.scali@student.tudelft.nl>
2019-12-03 13:04:58 +0100

    Discharge

---
+++ discharge/discharge sander/cross_sections.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 15:43:41 2019
+
+@author: tommyesse
+"""
+import numpy as np
+from general_data import * 
+
+print("Cross sections Imported \n")
